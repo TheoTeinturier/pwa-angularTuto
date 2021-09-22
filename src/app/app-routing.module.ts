187d239be3a87d0b8a320007b85d0b9565d395ad
Notsuko <teinturier.theo@gmail.com>
2021-09-22 12:48:06 +0200

    Tuto Angular PWA - Mise en place de la PWA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hello', component: HelloComponent },
+  { path: '', component: HelloComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
